0bd4529899f13c119af6a64ad940700d750bc259
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-02-03 17:02:56 +0200

    skeleton and color palette.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="created-by">Created by Brandon Parkerson</div>
+            <div>
+                <a href="https://github.com/brandon-parkerson/silkamo-library">Github</a>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Library.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Library() {
+    return (
+        <div className="library-container">
+            <ul>
+                <li>The Hobbit by J.R.R Tolkein | 300 pages | 5/5 <button className="edit-btn btn">Edit</button><button className="delete-btn btn">Delete</button></li>
+            </ul>
+            
+        </div>
+    )
+}
